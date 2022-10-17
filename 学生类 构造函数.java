d0270a59e7fb996637eb8dc3fc580e6ab95bfe13
ling <101567686+intling-luo@users.noreply.github.com>
2022-10-17 21:45:16 +0800

    Update 学生类 构造函数.java
    初次接触对象概念，写出这个题目的时候真的很激动！

--- 学生类 构造函数.java
+++ 学生类 构造函数.java
@@ -1,3 +1,22 @@
+// 定义一个有关学生的Student类，内含类成员变量：
+// String name、String sex、int age，所有的变量必须为私有(private)。
+
+// 1.编写有参构造函数：
+// 能对name,sex,age赋值。
+
+// 2.覆盖toString函数：
+
+// 按照格式：类名 [name=, sex=, age=]输出。使用idea自动生成,然后在修改成该输出格式
+
+// 3.对每个属性生成setter/getter方法
+
+// 4.main方法中
+
+// •输入1行name age sex , 调用上面的有参构造函数新建对象。
+
+
+
+
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
